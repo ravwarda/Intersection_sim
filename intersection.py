d85de677b3a38a97626b6cac26ab338325848cce
ravwarda <73530034+ravwarda@users.noreply.github.com>
2024-01-05 17:39:58 +0100

    started writing simulation process in intersection

--- intersection.py
+++ intersection.py
@@ -1,10 +1,37 @@
+from event import Event
+
+
 class Intersection:
-    def __init__(self):
-        self.sys_time = 0.0
+    def __init__(self, events):
+        self.sys_time = 0
         self.queues = [[], [], [], []]
         self.segments = []
         for i in range(0, 4):
             self.segments.append(self.Segment())
+        self.events = events
+
+    def run_sim(self, sim_time):
+        self.events.append(Event("end", sim_time, None))
+        while self.sys_time < sim_time:
+            self.sys_time = min(event.time for event in self.events)
+            in_events = []
+            out_events = []
+            for event in self.events:
+                if event.time == self.sys_time:
+                    if event.type == "in":
+                        in_events.append(event)
+                    else:
+                        out_events.append(event)
+                    self.events.remove(event)
+            for event in in_events:
+                # dodawanie aut do kolejek
+                pass
+            for event in out_events:
+                # zwalnianie sektorów
+                pass
+            # wpuszczanie aut na skrzyżowanie
+
+        return 0
 
     class Segment:
         def __init__(self):
---
+++ main.py
@@ -0,0 +1,7 @@
+from intersection import Intersection
+from event import Event
+
+# events = [Event("in", 54, 2), Event("out", 75, 1)]
+#
+# inter_sim = Intersection(events)
+# inter_sim.run_sim(100)
