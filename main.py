6a1e620f33e3f9b917533566b765a81e5f76deaa
Rafał Warda <W.rafi.X232@gmail.com>
2024-09-11 17:54:38 +0200

    finished GUI control panel

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ from tkinter import *
 from tkinter.ttk import *
 
 
-def execute_simulation(use_file=False):
+def execute_simulation(use_file=False, traffic_intensity = 2, segment_drive_time_distribution = 1, percentage_cars_on_main_road = 0.3):
     # ---Zmienne parametry pomiędzy symulacjami:
-    # natężenie ilości aut na całym skrzyżowaniu (czym mniejsza wartosc tym wiecej aut)
-    traffic_intensity = 2
-    # średnia dla rozkładu lognormalnego określająca czas przejechania przez segment skrzyżowania
-    segment_drive_time_distribution = 1
-    # ile procent aut przyjeżdża z jednego kierunku drogi głównej
-    percentage_cars_on_main_road = 0.3
+    # traffic_intensity - natężenie ilości aut na całym skrzyżowaniu (czym mniejsza wartosc tym wiecej aut)
+    # segment_drive_time_distribution - średnia dla rozkładu lognormalnego określająca czas przejechania przez segment skrzyżowania
+    # percentage_cars_on_main_road - ile procent aut przyjeżdża z jednego kierunku drogi głównej
+
 
     # ---Stałe parametry początkowe dla wszystkich symulacji:
     # czas trwania symulacji [s]
@@ -47,34 +45,39 @@ def execute_simulation(use_file=False):
 
 def menu():
     root = Tk()
-    # btn_ssim = Button(root, text="Przeprowadź symulację", command=execute_simulation)
-    # btn_summary = Button(root, text="Pokaż podsumowanie")
-    # btn_save = Button(root, text="Zapisz przebieg symulacji")
-    # btn_load = Button(root, text="Wczytaj przebieg symulacji")
-    #
-    # btn_ssim.grid(row=0, column=0, pady=3)
-    # btn_summary.grid(row=1, column=0, pady=3)
-    # btn_save.grid(row=2, column=0, pady=3)
-    # btn_load.grid(row=3, column=0, pady=3)
 
-    var = StringVar()
+    lbl_sim = Label(root, text="Ustaw wartości parametrów dla symulacji, lub użyj wartości z pliku 'parameters.json'")
 
     lbl_ti = Label(root, text="Średni czas pomiędzy przyjazdami samochodów:")
-    ent_ti = Entry(root)
+    ti_var = IntVar()
+    ti_var.set(2)
+    ent_ti = Entry(root, textvariable=ti_var)
+
     lbl_sd = Label(root, text="Średni czas przejechania przez segment:")
-    ent_sd = Entry(root)
+    sd_var = IntVar()
+    sd_var.set(1)
+    ent_sd = Entry(root, textvariable=sd_var)
+
     lbl_pc = Label(root, text="Ilość samochodów na drodze główniej (wartość: (0;0.5])")
-    ent_pc = Entry(root)
-    chk_fi = Checkbutton(root, text="Użyj pliku z wartościami parametrów", variable=var)
+    pc_var = DoubleVar()
+    pc_var.set(0.3)
+    ent_pc = Entry(root, textvariable=pc_var)
 
-    lbl_ti.grid(row=0, column=0, pady=3)
-    ent_ti.grid(row=1, column=0, pady=3)
-    lbl_sd.grid(row=2, column=0, pady=3)
-    ent_sd.grid(row=3, column=0, pady=3)
-    lbl_pc.grid(row=4, column=0, pady=3)
-    ent_pc.grid(row=5, column=0, pady=3)
-    chk_fi.grid(row=6, column=0, pady=5)
+    btn_ssim = Button(root, text="Przeprowadź symulację", command=lambda: execute_simulation(False, ti_var.get(), sd_var.get(), pc_var.get()))
 
+    btn_fsim = Button(root, text="Wyświetl analizę symulacji z parametrami z pliku", command=lambda: execute_simulation(True))
+
+    lbl_sim.grid(row=0, column=0, pady=5)
+
+    lbl_ti.grid(row=1, column=0, pady=3)
+    ent_ti.grid(row=2, column=0, pady=3)
+    lbl_sd.grid(row=3, column=0, pady=3)
+    ent_sd.grid(row=4, column=0, pady=3)
+    lbl_pc.grid(row=5, column=0, pady=3)
+    ent_pc.grid(row=6, column=0, pady=3)
+
+    btn_ssim.grid(row=7, column=0, pady=5)
+    btn_fsim.grid(row=8, column=0, pady=10)
 
     root.mainloop()
 
